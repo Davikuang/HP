5b693a67d58ec3cc573677a6b6ad9877e4bf0de0
kuangyanwei <kuangyanwei@puhuipiaoju.com>
2016-05-24 23:12:30 +0800

    Initial Commit

---
+++ HP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HP
+//
+//  Created by 匡 on 16/5/24.
+//  Copyright © 2016年 匡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HP
+//
+//  Created by 匡 on 16/5/24.
+//  Copyright © 2016年 匡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
